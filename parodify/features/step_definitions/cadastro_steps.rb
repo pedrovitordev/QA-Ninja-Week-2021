881267c3f33618a39abb88ccc1c02fb771e9f78d
pedrovitordev <69403840+pedrovitordev@users.noreply.github.com>
2022-03-09 14:45:22 -0300

    Consumindo a APi para excluir o usuario automatizado no ruby

--- parodify/features/step_definitions/cadastro_steps.rb
+++ parodify/features/step_definitions/cadastro_steps.rb
@@ -2,8 +2,11 @@ Dado('que acesso a página de cadastro') do
     visit "http://parodify.herokuapp.com/users/sign_up"
 end                                                                           
                                                                                 
-Quando('submete o meu cadastro com:') do |table|                              
+Quando('submete o meu cadastro com:') do |table| 
     user = table.rows_hash
+
+    delorean user[:email]
+
     find("#user_email").set user[:email]
     find("#user_password").set user[:senha]
     find("#user_password_confirmation").set user[:senha_confirma]
---
+++ parodify/features/support/helpers.rb
@@ -0,0 +1,9 @@
+require "httparty"
+
+module Helpers
+
+    def delorean(email)
+        HTTParty.get("http://parodify.herokuapp.com/helpers?email=#{email}")
+    end 
+
+end
