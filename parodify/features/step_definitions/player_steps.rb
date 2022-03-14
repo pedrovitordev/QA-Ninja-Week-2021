213c4611a9f2b1590a762d4c0afe4b6022bda791
pedrovitordev <69403840+pedrovitordev@users.noreply.github.com>
2022-03-14 12:41:11 -0300

    Finalizado a reprodução da parodia

--- parodify/features/step_definitions/player_steps.rb
+++ parodify/features/step_definitions/player_steps.rb
@@ -10,14 +10,20 @@ end
 Dado("que eu gosto muito de {string}") do |cat|
   find("a[href='/search/new']").click
   find("img[src$='#{cat.downcase}.png']").click
-
-  sleep 10
 end
 
 Quando("toco a seguinte canção:") do |table|
-  parodi = table.rows_hash
+  @parody = table.rows_hash
+
+  find("a", text: @parody[:banda]).click
+
+  song = find(".song-item", text: @parody[:parodia])
+  song.find(".fa-play-circle").click
 end
 
 Então("essa paródia deve ficar em modo de reprodução") do
-  pending # Write code here that turns the phrase above into concrete actions
+  song_play = find(".playing")
+  expect(song_play).to have_text @parody[:parodia]
+
+  sleep 10
 end
